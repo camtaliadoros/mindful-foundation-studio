f546aea47716bdc2f35988d1f4121bf6147f59f6
Cam Taliadoros <84043785+camtaliadoros@users.noreply.github.com>
2025-11-01 23:32:56 +0000

    updates

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -3,13 +3,14 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'ue9tzk13',
-    dataset: 'production'
+    dataset: 'production',
   },
   deployment: {
     /**
      * Enable auto-updates for studios.
      * Learn more at https://www.sanity.io/docs/cli#auto-updates
      */
+    appId: 'n4tx7rbxm3nxcwlx40j3w9l4',
     autoUpdates: true,
-  }
+  },
 })
